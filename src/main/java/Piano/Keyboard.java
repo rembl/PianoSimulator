7812e79e8bb2219cdc04267cd63ee1d1bb8f908f
rembl <58863120+rembl@users.noreply.github.com>
2021-05-17 23:15:03 +0300

    -

--- src/main/java/Piano/Keyboard.java
+++ src/main/java/Piano/Keyboard.java
@@ -8,220 +8,220 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
+import java.io.InputStream;
+import java.util.Random;
 
-import static Piano.PianoSimulator.background;
-import static Piano.PianoSimulator.header;
+import static Piano.PianoSimulator.*;
 
 public class Keyboard implements KeyListener {
+
+    final Random r = new Random();
 
     @Override
     public void keyTyped(KeyEvent e) {}
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
         int keyCode = e.getKeyCode();
         String musicPathName;
-        String picPathName;
-        String text;
 
         if(keyCode == KeyEvent.VK_ESCAPE) {
             System.exit(0);
         }
 
         if(keyCode == KeyEvent.VK_ENTER) {
-            ImageIcon back = new ImageIcon("src/main/resources/backgroundImage.png");
-            background.setIcon(new ImageIcon(back.getImage().getScaledInstance(1200, 400, Image.SCALE_SMOOTH)));
-            header.setText("Press any key, press ENTER to reset");
+            for (JButton button : buttons) {
+                button.setForeground(Color.BLACK);
+            }
         }
 
         if (keyCode == KeyEvent.VK_Z) {
-            musicPathName = "src/main/resources/29.wav";
-            picPathName = "src/main/resources/backgroundImageC1.png";
-            text = "Key pressed: C1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            C1.setForeground(color);
+            musicPathName = "/29.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_W) {
-            musicPathName = "src/main/resources/30.wav";
-            picPathName = "src/main/resources/backgroundImageC#1.png";
-            text = "Key pressed: C#1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            C11.setForeground(color);
+            musicPathName = "/30.wav";
+            PLay(musicPathName);
+
         }
 
         if(keyCode == KeyEvent.VK_X) {
-            musicPathName = "src/main/resources/31.wav";
-            picPathName = "src/main/resources/backgroundImageD1.png";
-            text = "Key pressed: D1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            D1.setForeground(color);
+            musicPathName = "/31.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_E) {
-            musicPathName = "src/main/resources/32.wav";
-            picPathName = "src/main/resources/backgroundImageD#1.png";
-            text = "Key pressed: D#1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            D11.setForeground(color);
+            musicPathName = "/32.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_C) {
-            musicPathName = "src/main/resources/33.wav";
-            picPathName = "src/main/resources/backgroundImageE1.png";
-            text = "Key pressed: E1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            E1.setForeground(color);
+            musicPathName = "/33.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_S) {
-            musicPathName = "src/main/resources/34.wav";
-            picPathName = "src/main/resources/backgroundImageF1.png";
-            text = "Key pressed: F1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            F1.setForeground(color);
+            musicPathName = "/34.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_R) {
-            musicPathName = "src/main/resources/35.wav";
-            picPathName = "src/main/resources/backgroundImageF#1.png";
-            text = "Key pressed: F#1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            F11.setForeground(color);
+            musicPathName = "/35.wav";
+            PLay(musicPathName);
+
         }
 
         if(keyCode == KeyEvent.VK_D) {
-            musicPathName = "src/main/resources/36.wav";
-            picPathName = "src/main/resources/backgroundImageG1.png";
-            text = "Key pressed: G1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            G1.setForeground(color);
+            musicPathName = "/36.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_T) {
-            musicPathName = "src/main/resources/37.wav";
-            picPathName = "src/main/resources/backgroundImageG#1.png";
-            text = "Key pressed: G#1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            G11.setForeground(color);
+            musicPathName = "/37.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_F) {
-            musicPathName = "src/main/resources/38.wav";
-            picPathName = "src/main/resources/backgroundImageA1.png";
-            text = "Key pressed: A1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            A1.setForeground(color);
+            musicPathName = "/38.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_Y) {
-            musicPathName = "src/main/resources/39.wav";
-            picPathName = "src/main/resources/backgroundImageA#1.png";
-            text = "Key pressed: A#1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            A11.setForeground(color);
+            musicPathName = "/39.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_G) {
-            musicPathName = "src/main/resources/40.wav";
-            picPathName = "src/main/resources/backgroundImageB1.png";
-            text = "Key pressed: B1";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            B1.setForeground(color);
+            musicPathName = "/40.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_H) {
-            musicPathName = "src/main/resources/41.wav";
-            picPathName = "src/main/resources/backgroundImageC2.png";
-            text = "Key pressed: C2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            C2.setForeground(color);
+            musicPathName = "/41.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_U) {
-            musicPathName = "src/main/resources/42.wav";
-            picPathName = "src/main/resources/backgroundImageC#2.png";
-            text = "Key pressed: C#2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            C22.setForeground(color);
+            musicPathName = "/42.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_J) {
-            musicPathName = "src/main/resources/43.wav";
-            picPathName = "src/main/resources/backgroundImageD2.png";
-            text = "Key pressed: D2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            D2.setForeground(color);
+            musicPathName = "/43.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_I) {
-            musicPathName = "src/main/resources/44.wav";
-            picPathName = "src/main/resources/backgroundImageD#2.png";
-            text = "Key pressed: D#2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            D22.setForeground(color);
+            musicPathName = "/44.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_K) {
-            musicPathName = "src/main/resources/45.wav";
-            picPathName = "src/main/resources/backgroundImageE2.png";
-            text = "Key pressed: E2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            E2.setForeground(color);
+            musicPathName = "/45.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_L) {
-            musicPathName = "src/main/resources/46.wav";
-            picPathName = "src/main/resources/backgroundImageF2.png";
-            text = "Key pressed: F2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            F2.setForeground(color);
+            musicPathName = "/46.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_O) {
-            musicPathName = "src/main/resources/47.wav";
-            picPathName = "src/main/resources/backgroundImageF#2.png";
-            text = "Key pressed: F#2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            F22.setForeground(color);
+            musicPathName = "/47.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == 59) {
-            musicPathName = "src/main/resources/48.wav";
-            picPathName = "src/main/resources/backgroundImageG2.png";
-            text = "Key pressed: G2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            G2.setForeground(color);
+            musicPathName = "/48.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_P) {
-            musicPathName = "src/main/resources/49.wav";
-            picPathName = "src/main/resources/backgroundImageG#2.png";
-            text = "Key pressed: G#2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            G22.setForeground(color);
+            musicPathName = "/49.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_M) {
-            musicPathName = "src/main/resources/50.wav";
-            picPathName = "src/main/resources/backgroundImageA2.png";
-            text = "Key pressed: A2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            A2.setForeground(color);
+            musicPathName = "/50.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == 91) {
-            musicPathName = "src/main/resources/51.wav";
-            picPathName = "src/main/resources/backgroundImageA#2.png";
-            text = "Key pressed: A#2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            A22.setForeground(color);
+            musicPathName = "/51.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == KeyEvent.VK_COMMA) {
-            musicPathName = "src/main/resources/52.wav";
-            picPathName = "src/main/resources/backgroundImageB2.png";
-            text = "Key pressed: B2";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            B2.setForeground(color);
+            musicPathName = "/52.wav";
+            PLay(musicPathName);
         }
 
         if(keyCode == 46) {
-            musicPathName = "src/main/resources/53.wav";
-            picPathName = "src/main/resources/backgroundImageC3.png";
-            text = "Key pressed: C3";
-            PLay(musicPathName, picPathName, text);
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            C3.setForeground(color);
+            musicPathName = "/53.wav";
+            PLay(musicPathName);
         }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {}
-
-    public void PLay(String musicPathName, String picPathName, String text) {
-        ImageIcon back = new ImageIcon(picPathName);
-        background.setIcon(new ImageIcon(back.getImage().getScaledInstance(1200, 400, Image.SCALE_SMOOTH)));
-        header.setText(text);
+    public void PLay(String musicPathName) {
+        InputStream s = this.getClass().getResourceAsStream(musicPathName);
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(musicPathName));
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(s);
             Clip clip = AudioSystem.getClip();
-            clip.stop();
             clip.open(audioInputStream);
             clip.start();
         } catch (Exception ex) {
--- src/main/java/Piano/PianoSimulator.java
+++ src/main/java/Piano/PianoSimulator.java
@@ -1,32 +1,82 @@
 package Piano;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
-public class PianoSimulator{
+public class PianoSimulator extends JPanel {
 
     static JFrame frame;
+    static JPanel radar;
     static JLabel header;
     static JLabel background;
-    ImageIcon back = new ImageIcon("src/main/resources/backgroundImage.png");
+    static JButton C1 = new JButton("C1");
+    static JButton C11 = new JButton("C#1");
+    static JButton D1 = new JButton("D1");
+    static JButton D11 = new JButton("D#1");
+    static JButton E1 = new JButton("E1");
+    static JButton F1 = new JButton("F1");
+    static JButton F11 = new JButton("F#1");
+    static JButton G1 = new JButton("G1");
+    static JButton G11 = new JButton("G#1");
+    static JButton A1 = new JButton("A1");
+    static JButton A11 = new JButton("A#1");
+    static JButton B1 = new JButton("B1");
+    static JButton C2 = new JButton("C2");
+    static JButton C22 = new JButton("C#2");
+    static JButton D2 = new JButton("D2");
+    static JButton D22 = new JButton("D#2");
+    static JButton E2 = new JButton("E2");
+    static JButton F2 = new JButton("F2");
+    static JButton F22 = new JButton("F#2");
+    static JButton G2 = new JButton("G2");
+    static JButton G22 = new JButton("G#2");
+    static JButton A2 = new JButton("A2");
+    static JButton A22 = new JButton("A#2");
+    static JButton B2 = new JButton("B2");
+    static JButton C3 = new JButton("C3");
+    static List<JButton> buttons;
+    String absName = "/backgroundImage.png";
+    InputStream s = this.getClass().getResourceAsStream(absName);
     Keyboard keyboard;
+
 
     public PianoSimulator() {
         frame = new JFrame("Piano Simulator");
-        frame.setSize(1200, 480);
+        frame.setSize(1200, 500);
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        header = new JLabel("Press any key, press ENTER to reset", SwingConstants.CENTER);
-        header.setFont(header.getFont().deriveFont(35.0f));
+        header = new JLabel("Press any key, press ENTER to reset colors", SwingConstants.CENTER);
+        header.setFont(header.getFont().deriveFont(30.0f));
         frame.add(header, BorderLayout.NORTH);
 
+        buttons = Arrays.asList(C1, C11, D1, D11, E1, F1, F11, G1, G11, A1, A11, B1, C2, C22, D2, D22, E2, F2, F22, G2, G22, A2, A22, B2, C3);
+        radar = new JPanel();
+        radar.setLayout(new GridLayout(1, 25, 5, 0));
+        for(JButton button : buttons) {
+            button.setFocusable(false);
+            radar.add(button);
+        }
+        frame.add(radar, BorderLayout.CENTER);
+
+
         background = new JLabel();
+        ImageIcon back = null;
+        try {
+            back = new ImageIcon(ImageIO.read(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         background.setIcon(new ImageIcon(back.getImage().getScaledInstance(1200, 400, Image.SCALE_SMOOTH)));
         frame.add(background, BorderLayout.SOUTH);
-
+        
         keyboard = new Keyboard();
         frame.addKeyListener(keyboard);
         frame.setFocusable(true);
