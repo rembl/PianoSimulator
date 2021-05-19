20d386769e7b479d41ec0b4e3a4a818fefdfa355
rembl <58863120+rembl@users.noreply.github.com>
2021-05-19 13:40:39 +0300

    -

--- src/main/java/Piano/Keyboard.java
+++ src/main/java/Piano/Keyboard.java
@@ -40,184 +40,140 @@ public class Keyboard implements KeyListener {
         }
 
         if (keyCode == KeyEvent.VK_Z) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            C1.setForeground(color);
             musicPathName = "/29.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, C1);
         }
 
         if(keyCode == KeyEvent.VK_W) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            C11.setForeground(color);
             musicPathName = "/30.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, C11);
 
         }
 
         if(keyCode == KeyEvent.VK_X) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            D1.setForeground(color);
             musicPathName = "/31.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, D1);
         }
 
         if(keyCode == KeyEvent.VK_E) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            D11.setForeground(color);
             musicPathName = "/32.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, D11);
         }
 
         if(keyCode == KeyEvent.VK_C) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            E1.setForeground(color);
             musicPathName = "/33.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, E1);
         }
 
         if(keyCode == KeyEvent.VK_S) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            F1.setForeground(color);
             musicPathName = "/34.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, F1);
         }
 
         if(keyCode == KeyEvent.VK_R) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            F11.setForeground(color);
             musicPathName = "/35.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, F11);
 
         }
 
         if(keyCode == KeyEvent.VK_D) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            G1.setForeground(color);
             musicPathName = "/36.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, G1);
         }
 
         if(keyCode == KeyEvent.VK_T) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            G11.setForeground(color);
             musicPathName = "/37.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, G11);
         }
 
         if(keyCode == KeyEvent.VK_F) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            A1.setForeground(color);
             musicPathName = "/38.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, A1);
         }
 
         if(keyCode == KeyEvent.VK_Y) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            A11.setForeground(color);
             musicPathName = "/39.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, A11);
         }
 
         if(keyCode == KeyEvent.VK_G) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            B1.setForeground(color);
             musicPathName = "/40.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, B1);
         }
 
         if(keyCode == KeyEvent.VK_H) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            C2.setForeground(color);
             musicPathName = "/41.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, C2);
         }
 
         if(keyCode == KeyEvent.VK_U) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            C22.setForeground(color);
             musicPathName = "/42.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, C22);
         }
 
         if(keyCode == KeyEvent.VK_J) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            D2.setForeground(color);
             musicPathName = "/43.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, D2);
         }
 
         if(keyCode == KeyEvent.VK_I) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            D22.setForeground(color);
             musicPathName = "/44.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, D22);
         }
 
         if(keyCode == KeyEvent.VK_K) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            E2.setForeground(color);
             musicPathName = "/45.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, E2);
         }
 
         if(keyCode == KeyEvent.VK_L) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            F2.setForeground(color);
             musicPathName = "/46.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, F2);
         }
 
         if(keyCode == KeyEvent.VK_O) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            F22.setForeground(color);
             musicPathName = "/47.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, F22);
         }
 
         if(keyCode == 59) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            G2.setForeground(color);
             musicPathName = "/48.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, G2);
         }
 
         if(keyCode == KeyEvent.VK_P) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            G22.setForeground(color);
             musicPathName = "/49.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, G22);
         }
 
         if(keyCode == KeyEvent.VK_M) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            A2.setForeground(color);
             musicPathName = "/50.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, A2);
         }
 
         if(keyCode == 91) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            A22.setForeground(color);
             musicPathName = "/51.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, A22);
         }
 
         if(keyCode == KeyEvent.VK_COMMA) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            B2.setForeground(color);
             musicPathName = "/52.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, B2);
         }
 
         if(keyCode == 46) {
-            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
-            C3.setForeground(color);
             musicPathName = "/53.wav";
-            PLay(musicPathName);
+            PLay(musicPathName, C3);
         }
+
     }
 
-    public void PLay(String musicPathName) {
+    public void PLay(String musicPathName, JButton button) {
+        Color old = button.getForeground();
+        while(button.getForeground() == Color.BLACK || button.getForeground() == old) {
+            Color color = new Color(r.nextInt(256),r.nextInt(256),r.nextInt(256),r.nextInt(256));
+            button.setForeground(color);
+        }
         InputStream s = this.getClass().getResourceAsStream(musicPathName);
         InputStream buffered = new BufferedInputStream(s);
         try {
--- src/main/java/Piano/PianoSimulator.java
+++ src/main/java/Piano/PianoSimulator.java
@@ -59,12 +59,111 @@ public class PianoSimulator extends JPanel {
 
         buttons = Arrays.asList(C1, C11, D1, D11, E1, F1, F11, G1, G11, A1, A11, B1, C2, C22, D2, D22, E2, F2, F22, G2, G22, A2, A22, B2, C3);
         radar = new JPanel();
-        radar.setLayout(new GridLayout(1, 25, 5, 0));
-        for(JButton button : buttons) {
-            button.setFocusable(false);
-            radar.add(button);
-        }
+        radar.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        C1.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 0;
+        c.gridy = 0;
+        radar.add(C1, c);
+        C11.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 1;
+        c.gridy = 0;
+        radar.add(C11, c);
+        D1.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 2;
+        c.gridy = 0;
+        radar.add(D1, c);
+        D11.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 3;
+        c.gridy = 0;
+        radar.add(D11, c);
+        E1.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 4;
+        c.gridy = 0;
+        radar.add(E1, c);
+        F1.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 5;
+        c.gridy = 0;
+        radar.add(F1, c);
+        F11.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 6;
+        c.gridy = 0;
+        radar.add(F11, c);
+        G1.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 7;
+        c.gridy = 0;
+        radar.add(G1, c);
+        G11.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 8;
+        c.gridy = 0;
+        radar.add(G11, c);
+        A1.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 9;
+        c.gridy = 0;
+        radar.add(A1, c);
+        A11.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 10;
+        c.gridy = 0;
+        radar.add(A11, c);
+        B1.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 11;
+        c.gridy = 0;
+        radar.add(B1, c);
+        C2.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 12;
+        c.gridy = 0;
+        radar.add(C2, c);
+        C22.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 13;
+        c.gridy = 0;
+        radar.add(C22, c);
+        D2.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 14;
+        c.gridy = 0;
+        radar.add(D2, c);
+        D22.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 15;
+        c.gridy = 0;
+        radar.add(D22, c);
+        E2.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 16;
+        c.gridy = 0;
+        radar.add(E2, c);
+        F2.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 17;
+        c.gridy = 0;
+        radar.add(F2, c);
+        F22.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 18;
+        c.gridy = 0;
+        radar.add(F22, c);
+        G2.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 19;
+        c.gridy = 0;
+        radar.add(G2, c);
+        G22.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 20;
+        c.gridy = 0;
+        radar.add(G22, c);
+        A2.setPreferredSize(new Dimension(38, 40));
+        c.gridx = 21;
+        c.gridy = 0;
+        radar.add(A2, c);
+        A22.setPreferredSize(new Dimension(45, 40));
+        c.gridx = 22;
+        c.gridy = 0;
+        radar.add(A22, c);
+        B2.setPreferredSize(new Dimension(55, 40));
+        c.gridx = 23;
+        c.gridy = 0;
+        radar.add(B2, c);
+        C3.setPreferredSize(new Dimension(80, 40));
+        c.gridx = 24;
+        c.gridy = 0;
+        radar.add(C3, c);
         frame.add(radar, BorderLayout.CENTER);
+
+
 
 
         background = new JLabel();
@@ -84,7 +183,6 @@ public class PianoSimulator extends JPanel {
         frame.setVisible(true);
 
     }
-
 
     public static void main(String[] args) {
         PianoSimulator test = new PianoSimulator();
